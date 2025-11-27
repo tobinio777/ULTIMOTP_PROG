066a407fb4294298b0cb257ade5cec910e4ec403
tobinio777 <tobiasnatanaeltomadin@gmail.com>
2025-11-27 19:44:07 -0300

    solo falta la documentacion

--- front/src/components/iu/ProductsList.jsx
+++
@@ -1,9 +0,0 @@
-import { ProductRow } from '../ProductRow'
-
-export const ProductsList = ({ data, onDelete }) => {
-    return (
-        <div className="flex flex-col gap-4">
-            {data.map((d) => <ProductRow key={d.id} data={d} onDelete={onDelete} />)}
-        </div>
-    )
-}
