f764407eacb9cb2c5bb31c181220091aef39d8e3
tobinio777 <tobiasnatanaeltomadin@gmail.com>
2025-11-27 19:33:33 -0300

    solo falta la documentacion

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -9,39 +9,39 @@ import { ToastContainer } from "react-toastify"
 import Navbar from './components/Navbar'
 import CartPage from './components/CartPage'
 import Checkout from './components/Checkout'
-import TicketPurchase from './components/TicketPurchase'
+
 
 const App = () => {
   return (
     <BrowserRouter>
-      {/* Fondo global para toda la aplicación */}
+      
       <div 
         className="min-h-screen bg-cover bg-center bg-fixed"
         style={{ backgroundImage: "url('/deposito.jpg')" }}
       >
-        {/* Overlay oscuro */}
+        
         <div className="min-h-screen bg-black bg-opacity-40">
           <Navbar />
           
           <Routes>
-            {/* Public routes */}
+            
             <Route element={<Public />} path="/">
               <Route index element={<ProductList />} />
               <Route path="login" element={<Login />} />
               <Route path="register" element={<Register />} />
             </Route>
 
-            {/* Private routes */}
+            
             <Route element={<Private />} path="/private">
               <Route index element={<ProductList />} />
               <Route path="product/new" element={<ProductForm />} />
               <Route path="product/edit/:id" element={<ProductForm />} />
             </Route>
 
-            {/* Cart, Checkout & Ticket */}
+            
             <Route path="/cart" element={<CartPage />} />
             <Route path="/checkout" element={<Checkout />} />
-            <Route path="/ticket" element={<TicketPurchase />} />
+            
 
             <Route path="*" element={<h1 className="text-white text-center text-3xl mt-20">404 - Página no encontrada</h1>} />
           </Routes>
